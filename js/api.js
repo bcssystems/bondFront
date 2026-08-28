09992dffd87576160fc49012be75161ee72f4d5c
BenyiCordero <benyi.uriel2006@gmail.com>
2026-08-28 11:22:51 -0600

    Se arregla base url

--- js/api.js
+++ js/api.js
@@ -1,5 +1,5 @@
 const API = {
-  baseUrl: 'http://localhost:8025/api/v1',
+  baseUrl: '/api/v1',
   _mediaBaseUrl: undefined,
   get mediaBaseUrl() {
     if (this._mediaBaseUrl !== undefined) return this._mediaBaseUrl;
